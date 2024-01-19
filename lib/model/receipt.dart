9c48dc36d30b28dc80f10be2ceeac7e388e9277a
Atuoha Anthony <atuohainitiatives@gmail.com>
2024-01-19 11:34:35 +0100

    full package install and model setup

---
+++ lib/model/receipt.dart
@@ -0,0 +1,11 @@
+class Receipt {
+  final String id;
+  final String name;
+  final int amount;
+
+  const Receipt({
+    required this.id,
+    required this.name,
+    required this.amount,
+  });
+}
